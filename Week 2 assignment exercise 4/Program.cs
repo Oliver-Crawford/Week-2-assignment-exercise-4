9f910615197bc0f9f99a6ad56d1f0740406d5619
Oliver Crawford <oicrawford@gmail.com>
2024-01-27 00:06:41 -0600

    Add project files.

---
+++ Week 2 assignment exercise 4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2_assignment_exercise_4
+{
+    internal class Program
+    { 
+        static void Main(string[] args)
+        {
+            /*Write an application which let user input random letters until user input 0.
+            * Then, the application will print out the list of letters associates which how many times they are input. */
+
+        }
+    }
+}
