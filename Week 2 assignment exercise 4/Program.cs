706a89e41c25ddb636f267183a830e08296fc712
Oliver Crawford <oicrawford@gmail.com>
2024-01-27 00:36:27 -0600

    Finished exercise 4

--- Week 2 assignment exercise 4/Program.cs
+++ Week 2 assignment exercise 4/Program.cs
@@ -12,7 +12,46 @@ namespace Week_2_assignment_exercise_4
         {
             /*Write an application which let user input random letters until user input 0.
             * Then, the application will print out the list of letters associates which how many times they are input. */
-
+            Dictionary<string, int> characters = new Dictionary<string, int>();
+            bool done = false;
+            bool checkDone = false;
+            string tempStr = "";
+            Console.WriteLine("Input characters, input 0 to end");
+            while (!done)
+            {
+                Console.Write("Please input a single Character: ");
+                while (!checkDone)
+                {
+                    tempStr = Console.ReadLine();
+                    if(tempStr.Length > 1 || tempStr.Length < 1)
+                    {
+                        Console.Write("Invalid input, try again: ");
+                    } else
+                    {
+                        checkDone = true;
+                    }
+                }
+                checkDone = false;
+                if(tempStr != "0")
+                {
+                    try
+                    {
+                        characters.Add(tempStr, 1);
+                    } catch
+                    {
+                        characters[tempStr] += 1;
+                    }
+                } else
+                {
+                    done = true;
+                }
+            }
+            Console.WriteLine("Characters : Times Written");
+            foreach(string i in characters.Keys)
+            {
+                Console.WriteLine($"{i} : {characters[i]}");
+            }
+            Console.ReadLine();
         }
     }
 }
